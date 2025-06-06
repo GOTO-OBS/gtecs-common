448ac35fdae4b8cff9cc16bfe791a5f5d6f031f2
Martin Dyer <martin.dyer@sheffield.ac.uk>
2025-06-06 13:46:17 +0100

    Add database requirements as optional

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ Note this module is Python3 only and has been developed for Linux, otherwise use
 
 This package requires several Python modules, which should be included during installation.
 
+Database support is optional, but if you want to use it, you will need to install `postgresql`
+first, and then install this package with the optional `db` flag (see below).
+
 This package doesn't require any other G-TeCS packages to be installed, but it itself is a requirement of several of them.
 
 ## Installation
@@ -17,6 +20,10 @@ This package doesn't require any other G-TeCS packages to be installed, but it i
 Once you've downloaded or cloned the repository, in the base directory run:
 
     pip3 install . --user
+
+If you want to install the package with database support, run:
+
+    pip3 install .[db] --user
 
 You should then be able to import the module from within Python.
 
--- setup.py
+++ setup.py
@@ -2,21 +2,25 @@
 from setuptools import find_namespace_packages, setup
 
 
-REQUIRES = ['slack_sdk>=3.20.1',
-            'configobj',
-            'fabric',
-            'pid',
-            # 'psycopg2',  # requires postgresql to be installed, requirement for obs/alert only
-            'sqlalchemy>=2',
-            ]
-
 setup(name='gtecs-common',
-      version='0',
-      description='G-TeCS common package',
-      url='http://github.com/GOTO-OBS/gtecs-common',
-      author='Martin Dyer',
-      author_email='martin.dyer@sheffield.ac.uk',
-      install_requires=REQUIRES,
-      packages=find_namespace_packages(include=['gtecs*']),
-      zip_safe=False,
-      )
+    version='0',
+    description='G-TeCS common package',
+    url='http://github.com/GOTO-OBS/gtecs-common',
+    author='Martin Dyer',
+    author_email='martin.dyer@sheffield.ac.uk',
+    install_requires=[
+        'slack_sdk>=3.20.1',
+        'configobj',
+        'fabric',
+        'pid',
+    ],
+    extras_require={
+        'db': [
+            # Optional dependencies for database support
+            'psycopg2',
+            'sqlalchemy>=2',
+        ]
+    },
+    packages=find_namespace_packages(include=['gtecs*']),
+    zip_safe=False,
+    )
